cb3181b6e9d1dcfb0a743f5edab4805d54171d1f
Kyle Burton <kyle.burton@gmail.com>
2014-11-03 11:23:23 -0500

    fixes #3

--- test/lib/mylib.sh
+++ test/lib/mylib.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
+
+bake_task bar_task "The bar_task command just echos it's arguments"
+function bar_task () {
+  echo "bar_task: args='$@'"
+}
+
 bake_task foo "The foo command just echos it's arguments"
 function foo () {
   echo "foo: args='$@'"
