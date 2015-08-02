d621565ff62ef571f4cc178ff9b793436a1ccb84
Kyle Burton <kyle.burton@gmail.com>
2015-08-02 10:12:57 -0400

    remove tmp file

--- bake-completion.sh
+++ bake-completion.sh
@@ -3,7 +3,6 @@
 _bake () {
   local cur=${COMP_WORDS[COMP_CWORD]}
   local tasks="$(bake | grep -v BAKEFILE | grep '^  ' | sed 's/^  //g' | cut -f1 -d' ')"
-  echo "TASKS: $tasks" >> x
   COMPREPLY=( $(compgen -W "$tasks" -- $cur) )
 }
 
