7ec59a8982552b93dd3abfaf90c5d1d3da2f6702
Kyle Burton <kyle.burton@gmail.com>
2018-07-01 12:58:12 -0700

    fixes #28

--- bake-completion.sh
+++ bake-completion.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
 _bake () {
-  local cur=${COMP_WORDS[COMP_CWORD]}
+  local cur
+  _get_comp_words_by_ref -n : -c cur
   local tasks="$(bake | grep -v BAKEFILE | grep '^  ' | sed 's/^  //g' | cut -f1 -d' ')"
   COMPREPLY=( $(compgen -W "$tasks" -- $cur) )
+
+  # NB: this is a workaround for bash's default of splitting words on colons
+  # when bake tasks also use colons as a namespace separator.  What we need to
+  # do is fill COMPREPLY with the remaining parts of the task after the last
+  # colon.
+  # TODO: as is, this fails to drop incomplete tasks
+  if [[ $cur == *:* ]]; then
+    local pfx="${cur%%:*}:"
+    for ((ii = 0; ii < ${#COMPREPLY[@]}; ++ii)); do
+      local w="${COMPREPLY[$ii]}"
+      COMPREPLY[$ii]="${w#$pfx}"
+    done
+  fi
+
 }
 
 
