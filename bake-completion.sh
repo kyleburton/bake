cf2f6e5a9ae5f65fdc8a76ad8c71e111b21541aa
Isaac Schaaf <ischaaf@riotgames.com>
2019-01-11 13:28:16 -0800

    only complete the first arg to bake with a bake task, fallback on default completion after that

--- bake-completion.sh
+++ bake-completion.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 _bake () {
-  local cur
-  _get_comp_words_by_ref -n : -c cur
-  local tasks="$(bake | grep -v BAKEFILE | grep '^  ' | sed 's/^  //g' | cut -f1 -d' ')"
-  COMPREPLY=( $(compgen -W "$tasks" -- $cur) )
+  local cword cur tasks
+  _get_comp_words_by_ref -n : -c cur cword
+  tasks="$(bake | grep -v BAKEFILE | grep '^  ' | sed 's/^  //g' | cut -f1 -d' ')"
+  if [[ "$cword" -eq "1" ]]; then
+    mapfile -t COMPREPLY < <(compgen -W "$tasks" -- "$cur")
+  else
+    mapfile -t COMPREPLY < <(compgen -o default -- "$cur")
+  fi
 
   # NB: this is a workaround for bash's default of splitting words on colons
   # when bake tasks also use colons as a namespace separator.  What we need to
