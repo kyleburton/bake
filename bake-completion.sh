76451e52cc700b7a75e34bca014c853bb8b9cd7a
Isaac Schaaf <zeekus99@gmail.com>
2019-01-13 16:02:53 -0800

    WIP working on interface for completions

--- bake-completion.sh
+++ bake-completion.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 _bake () {
-  local cword cur tasks
-  _get_comp_words_by_ref -n : -c cur cword
+  local cword cur tasks words
+  _get_comp_words_by_ref -n : -c cur cword words
   tasks="$(bake | grep -v BAKEFILE | grep '^  ' | sed 's/^  //g' | cut -f1 -d' ')"
   if [[ "$cword" -eq "1" ]]; then
     mapfile -t COMPREPLY < <(compgen -W "$tasks" -- "$cur")
   else
-    mapfile -t COMPREPLY < <(compgen -o default -- "$cur")
+    source Bakefile.completers
+    local completion_fn="${words[1]}" # task name
+    mapfile -t COMPREPLY < <("$completion_fn.bake-completion" "$cur")
   fi
 
   # NB: this is a workaround for bash's default of splitting words on colons
