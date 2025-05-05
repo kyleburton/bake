7d1224aad7c1a3871d0d66af013d25c7b761774c
Kyle Burton <kyle.burton@gmail.com>
2025-05-05 11:07:10 -0700

    shellcheck all files (#43)
    * shellcheck all files

--- test/lib/mylib.sh
+++ test/lib/mylib.sh
@@ -4,15 +4,15 @@ set -eu
 
 bake_task bar_task "The bar_task command just echos its arguments"
 function bar_task () {
-  echo "bar_task: args='$@'"
+  echo "bar_task: args='$*'"
 }
 
 bake_task qux
 function qux () {
-  echo "qux: args='$@'"
+  echo "qux: args='$*'"
 }
 
 bake_task foo "The foo command just echos its arguments"
 function foo () {
-  echo "foo: args='$@'"
+  echo "foo: args='$*'"
 }
